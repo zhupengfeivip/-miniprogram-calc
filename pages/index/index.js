e30de355ec134d1f0973c33f7c70e495a2a12fdc
zhupengfei <zhupengfei@yorkg.com>
2019-07-25 22:27:15 +0800

    1.0。3修改为黄页
    1.0。3修改为黄页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,150 +4,61 @@ const app = getApp()
 
 Page({
   data: {
-    idb: "back",
-    idc: "clear",
-    idt: "toggle",
-    idadd: "＋",
-    id9: "9",
-    id8: "8",
-    id7: "7",
-    idj: "－",
-    id6: "6",
-    id5: "5",
-    id4: "4",
-    idx: "×",
-    id3: "3",
-    id2: "2",
-    id1: "1",
-    iddiv: "÷",
-    id0: "0",
-    idd: ".",
-    ide: "＝",
-    screenData: "0",
-    operaSymbo: { "＋": "+", "－": "-", "×": "*", "÷": "/", ".": "." },
-    lastIsOperaSymbo: false,
-    iconType: 'waiting_circle',
-    iconColor: 'white',
-    arr: [],
-    logs: []
-  },
-  onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  },
-  clickBtn: function (event) {
-    var id = event.target.id;
-    if (id == this.data.idb) {  //退格←
-      var data = this.data.screenData;
-      if (data == "0") {
-        return;
+    list: [
+      {
+        id: 'view',
+        name: '紧急电话',
+        open: false,
+        subName: ['火警', '盗警', '急救', '报时', '电力抢修', '管道液化气抢修', '市话障碍', '交通事故', '天气预报', '号码查询'],
+        phone: ['119', '110', '120', '117', '95598', '87221599', '112', '122', '121', '114']
+      }, {
+        id: 'form',
+        name: '银行电话',
+        open: false,
+        subName: ['工商银行', '建设银行', '农业银行', '中国银行', '交通银行', '浦发银行', '民生银行', '兴业银行', '中信银行', '深圳发展银行', '华夏银行', '招商银行', '广发银行', '广东农信', '光大银行'],
+        phone: ['95588', '95533', '95599', '95566', '95559', '95528', '95568', '95561', '95558', '95501', '95577', '95555', '95508', '96138', '95595']
+      }, {
+        id: 'feedback',
+        name: '快递电话',
+        open: false,
+        subName: ['申通快递', 'EMS', '顺丰速运', '	圆通速递', '中通速递', '韵达快运', '天天快递', '汇通快运', '速尔快递', '德邦物流', '中铁快运', '鑫飞鸿快递', 'UPS', 'FedEx(联邦快递)'],
+        phone: ['4008895543', '4008100999', '021-69777888', '021-39777777', '021-39207888', '021-67662333', '021-62963636', '4008822168', '4008305555', '95572', '021-69781999', '4008208388', '4008861888']
+      }, {
+        id: 'nav',
+        name: '通讯客服',
+        open: false,
+        subName: ['中国移动', '中国联通', '中国电信', '中国网通', '中国铁通', '中国邮政'],
+        phone: ['10086', '10010', '10000', '10060', '10050', '11185']
+      }, {
+        id: 'media',
+        name: '投诉举报',
+        open: false,
+        subName: ['消费者投诉热线', '价格投诉热线', '质量投诉', '环保投诉', '税务投诉', '公共卫生监督', '电信投诉', '市长热线', '法律援助', '妇女维权', '民工维权'],
+        phone: ['12315', '12358', '12365', '12369', '12366', '12320', '12300', '12366', '12351', '12338', '12333']
+      }, {
+        id: 'map',
+        name: '铁路航空',
+        subName: ['铁路', '国航', '海航', '南航', '东航', '深航', '厦航', '山航'],
+        phone: ['12306', '4008100999', '950718', '4006695539', '95530', '4008895080', '95557', '4006096777']
       }
-      data = data.substring(0, data.length - 1);
-      if (data == "" || data == "－") {
-        data = 0;
-      }
-      this.setData({ "screenData": data });
-      this.data.arr.pop();
-    } else if (id == this.data.idc) {  //清屏C
-      this.setData({ "screenData": "0" });
-      this.data.arr.length = 0;
-    } else if (id == this.data.idt) {  //正负号+/-
-      var data = this.data.screenData;
-      if (data == "0") {
-        return;
-      }
-      var firstWord = data.charAt(0);
-      if (data == "－") {
-        data = data.substr(1);
-        this.data.arr.shift();
+    ]
+  },
+  widgetsToggle: function (e) {
+    var id = e.currentTarget.id, list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open;
       } else {
-        data = "－" + data;
-        this.data.arr.unshift("－");
-      }
-      this.setData({ "screenData": data });
-    } else if (id == this.data.ide) {  //等于＝
-      var data = this.data.screenData;
-      if (data == "0") {
-        return;
-      }
-      //eval是js中window的一个方法，而微信页面的脚本逻辑在是在JsCore中运行，JsCore是一个没有窗口对象的环境，所以不能再脚本中使用window，也无法在脚本中操作组件                 
-      //var result = eval(newData);           
-      var lastWord = data.charAt(data.length);
-      if (isNaN(lastWord)) {
-        return;
-      }
-      var num = "";
-      var lastOperator = "";
-      var arr = this.data.arr;
-      var optarr = [];
-      for (var i in arr) {
-        if (isNaN(arr[i]) == false || arr[i] == this.data.idd || arr[i] == this.data.idt) {
-          num += arr[i];
-        } else {
-          lastOperator = arr[i];
-          optarr.push(num);
-          optarr.push(arr[i]);
-          num = "";
-        }
-      }
-      optarr.push(Number(num));
-      var result = Number(optarr[0]) * 1.0;
-      console.log(result);
-      for (var i = 1; i < optarr.length; i++) {
-        if (isNaN(optarr[i])) {
-          if (optarr[1] == this.data.idadd) {
-            result += Number(optarr[i + 1]);
-          } else if (optarr[1] == this.data.idj) {
-            result -= Number(optarr[i + 1]);
-          } else if (optarr[1] == this.data.idx) {
-            result *= Number(optarr[i + 1]);
-          } else if (optarr[1] == this.data.iddiv) {
-            result /= Number(optarr[i + 1]);
-          }
-        }
-      }
-      //存储历史记录
-      this.data.logs.push(data + '=' + result);
-      wx.setStorageSync("calclogs", this.data.logs);
-      this.data.arr.length = 0;
-      this.data.arr.push(result);
-      this.setData({ "screenData": result + "" });
-    } else {
-      if (this.data.operaSymbo[id]) { //如果是符号+-*/
-        if (this.data.lastIsOperaSymbo || this.data.screenData == "0") {
-          return;
-        }
-      }
-      var sd = this.data.screenData;
-      var data;
-      if (sd == 0) {
-        data = id;
-      } else {
-        data = sd + id;
-      }
-      this.setData({ "screenData": data });
-      this.data.arr.push(id);
-      if (this.data.operaSymbo[id]) {
-        this.setData({ "lastIsOperaSymbo": true });
-      } else {
-        this.setData({ "lastIsOperaSymbo": false });
+        list[i].open = false;
       }
     }
+    this.setData({
+      list: list
+    });
   },
-  history: function () {
-    wx.navigateTo({
-      url: '../history/history'
+  callPhone: function (e) {
+    wx.makePhoneCall({
+      phoneNumber: e.target.dataset.phone
     })
   }
-})
+});
